6bc0c04340920f9905e979d91c5444324140bd3b
Alexander Zukov <linuzzz7@yandex.ru>
2020-02-02 19:15:39 +0300

    Отладка программы в IDEA [#159464]

---
+++ chapter_001/src/test/java/SqMaxTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SqMaxTest {
+    @Test
+    public void distance1() {
+        int a = 6, b = 7, c = 3, d = 5;
+        int expected = 6;
+        int out = SqMax.max(a,b,c,d);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void distance2() {
+        int a = 2, b = 4, c = 7, d = 5;
+        int expected = 4;
+        int out = SqMax.max(a,b,c,d);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void distance3() {
+        int a = 9, b = 4, c = 7, d = 5;
+        int expected = 7;
+        int out = SqMax.max(a,b,c,d);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void distance4() {
+        int a = 2, b = 4, c = 2, d = 5;
+        int expected = 5;
+        int out = SqMax.max(a,b,c,d);
+        Assert.assertEquals(expected, out);
+    }
+}
