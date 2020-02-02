c0a7e2333ff7a5b1c6863fb367b8c90fe865539f
Alexander Zukov <linuzzz7@yandex.ru>
2020-02-02 19:56:26 +0300

    Отладка программы в IDEA [#159464] разобрался, программу изменил, тесты переписал

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -6,15 +6,17 @@ public class SqMax {
         if (first > second) {
             if (first > third) {
                 if (first > forth) {
-                    result = third;
+                    result = first;
                 }
+            } else if (third > forth) {
+                result = third;
             }
         } else if (second > third) {
             if (second > forth) {
-                result = first;
+                result = second;
             }
         } else if (third > forth) {
-            result = second;
+            result = third;
         }
         return result;
     }
--- chapter_001/src/test/java/SqMaxTest.java
+++ chapter_001/src/test/java/SqMaxTest.java
@@ -7,31 +7,31 @@ public class SqMaxTest {
     @Test
     public void distance1() {
         int a = 6, b = 4, c = 7, d = 5;
-        int expected = 5;
+        int expected = 7;
         int out = SqMax.max(a, b, c, d);
         Assert.assertEquals(expected, out);
     }
 
     @Test
     public void distance2() {
-        int a = 6, b = 7, c = 3, d = 5;
-        int expected = 6;
-        int out = SqMax.max(a, b, c, d);
-        Assert.assertEquals(expected, out);
-    }
-
-    @Test
-    public void distance3() {
-        int a = 2, b = 4, c = 7, d = 5;
+        int a = 1, b = 2, c = 4, d = 3;
         int expected = 4;
         int out = SqMax.max(a, b, c, d);
         Assert.assertEquals(expected, out);
     }
 
     @Test
+    public void distance3() {
+        int a = 1, b = 5, c = 2, d = 3;
+        int expected = 5;
+        int out = SqMax.max(a, b, c, d);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
     public void distance4() {
-        int a = 9, b = 4, c = 7, d = 5;
-        int expected = 7;
+        int a = 10, b = 2, c = 1, d = 3;
+        int expected = 10;
         int out = SqMax.max(a, b, c, d);
         Assert.assertEquals(expected, out);
     }
