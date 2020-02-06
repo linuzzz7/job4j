8c13cab372d2f17ac20a7d27a85517e2189832ec
Alexander Zukov <linuzzz7@yandex.ru>
2020-02-06 21:21:34 +0300

    6.0.2. Заполнение массива. [#226300]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,13 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Elena";
+        names[1] = "Dima";
+        names[2] = "Alex";
+        names[3] = "Victor";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
