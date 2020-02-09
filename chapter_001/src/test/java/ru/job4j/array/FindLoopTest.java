a889fbbd500851f19864a882d2e4692c379ef52b
Alexander Zukov <linuzzz7@yandex.ru>
2020-02-09 23:05:02 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,27 +10,51 @@ public class FindLoopTest {
     @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = 0;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenArrayHas7Then3() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {10, 56, 7, 10, 3, 21};
+        int[] input = new int[]{10, 56, 7, 10, 3, 21};
         int value = 7;
         int result = find.indexOf(input, value);
         int expect = 2;
         assertThat(result, is(expect));
     }
+
     @Test
     public void whenArrayNoHas30Then1() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {1, 7, 8, 2, 3, 21};
+        int[] input = new int[]{1, 7, 8, 2, 3, 21};
         int value = 30;
         int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind4() {
+        int[] input = new int[]{5, 2, 10, 2, 4};
+        int value = 4;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = -1;
         assertThat(result, is(expect));
     }
