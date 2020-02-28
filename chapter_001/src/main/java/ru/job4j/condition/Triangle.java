dc3b197041fcbadd56b9cc5b155f15278690cbdb
Alexander Zukov <linuzzz7@yandex.ru>
2020-02-28 17:53:28 +0300

    2.2. Рефакторинг - Площадь треугольника.

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,40 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
-        return ab + ac > bc && ab + bc > ac;
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     */
+    public boolean exist(double a, double c, double b) {
+        boolean result = a + b > c || a + c > b || b + c > a;
+        return result;
+    }
+
+    /**
+     * Метод вычисления периметра по длинам сторон.
+     */
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     */
+    public double area(double a, double b, double c) {
+        Triangle tri = new Triangle(first, second, third);
+        if (tri.exist(a, b, c)) {
+            return TrgArea.area(a, b, c);
+        } else {
+            return -1;
+        }
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -8,13 +9,43 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        Point first = new Point(0, 0);
+        Point second = new Point(1, 3);
+        Point third = new Point(4, 5);
+        Triangle tri = new Triangle(first, second, third);
+        boolean result = tri.exist(first.distance(second), second.distance(third), third.distance(first));
         assertThat(result, is(true));
     }
 
     @Test
     public void doesNotExist() {
-        boolean result = Triangle.exist(1.0, 2.0, 5.0);
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 0);
+        Point third = new Point(0, 0);
+        Triangle tri = new Triangle(first, second, third);
+        boolean result = tri.exist(first.distance(second), second.distance(third), third.distance(first));
         assertThat(result, is(false));
+    }
+
+    @Test
+    public void perimeter() {
+        Point first = new Point(0, 0);
+        Point second = new Point(1, 3);
+        Point third = new Point(4, 5);
+        Triangle tri = new Triangle(first, second, third);
+        double result = tri.period(first.distance(second), second.distance(third), third.distance(first));
+        double expected = 6.58;
+        Assert.assertEquals(expected, result, 0.01);
+    }
+
+    @Test
+    public void area() {
+        Point first = new Point(0, 0);
+        Point second = new Point(1, 3);
+        Point third = new Point(4, 5);
+        Triangle tri = new Triangle(first, second, third);
+        double result = tri.area(first.distance(second), second.distance(third), third.distance(first));
+        double expected = 3.5;
+        Assert.assertEquals(expected, result, 0.01);
     }
 }
