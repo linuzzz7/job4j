0ccba65c0b43989c0022df3230c5c4ecb5627e72
Alexander Zukov <linuzzz7@yandex.ru>
2020-03-05 12:41:24 +0300

    5. Реализовать класс Tracker

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,57 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int position = 0;
+
+    public Item add(Item item) {
+        item.setId(generateId());
+        items[position++] = item;
+        return item;
+    }
+
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    public Item[] findAll() {
+        Item[] itemWithoutNull = new Item[this.items.length];
+        int size = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] != null) {
+                itemWithoutNull[size] = this.items[i];
+                size++;
+            }
+        }
+        itemWithoutNull = Arrays.copyOf(itemWithoutNull, size);
+        return itemWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] temp = new Item[this.items.length];
+        int size = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] != null && this.items[i].getName().equals(key)) {
+                temp[size] = this.items[i];
+                size++;
+            }
+        }
+        temp = Arrays.copyOf(temp, size);
+        return temp;
+    }
+
+    public Item findById(String id) {
+        Item temp = null;
+        for (int i = 0; i < this.items.length; i++) {
+            if (this.items[i] != null && this.items[i].getId().equals(id)) {
+                temp = this.items[i];
+            }
+        }
+        return temp;
+    }
 }
