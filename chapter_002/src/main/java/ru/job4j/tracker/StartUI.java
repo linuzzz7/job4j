f6b9bbbc4ff9bd6f01ce228e99c6c9de3797986b
Alexander Zukov <linuzzz7@yandex.ru>
2020-04-27 12:18:34 +0300

    8. Реализация меню за счет шаблона стратегия. Доработал

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -14,13 +14,6 @@ public class StartUI {
 
     private void showMenu(UserAction[] actions) {
         System.out.println("Menu.");
-        System.out.println("0. Add new Item");
-        System.out.println("1. Show all items");
-        System.out.println("2. Edit item");
-        System.out.println("3. Delete item");
-        System.out.println("4. Find item by Id");
-        System.out.println("5. Find items by name");
-        System.out.println("6. Exit Program");
         for (int index = 0; index < actions.length; index++) {
             System.out.println(index + ". " + actions[index].name());
         }
@@ -29,9 +22,8 @@ public class StartUI {
     public static void main(String[] args) {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction()
-        };
+        UserAction[] actions = {new CreateAction(), new FindAllAction(), new ReplaceAction(),
+                new DeleteAction(), new FindByIdAction(), new FindByNameAction(), new ExitAction()};
         new StartUI().init(input, tracker, actions);
     }
 }
